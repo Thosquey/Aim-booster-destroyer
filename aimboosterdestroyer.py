92d85c55b0325e28ed5ba45c310a024f1f7f5c6c
Clément Guibout <clement.guibout@gmail.com>
2020-11-09 15:19:05 +0100

    Update
    Removed useless stuff and added comments

--- aimboosterdestroyer.py
+++ aimboosterdestroyer.py
@@ -1,7 +1,6 @@
 import keyboard
 import pyautogui
 import win32api, win32con
-import time
 
 MIN_X = 680
 MIN_Y = 350
@@ -13,12 +12,12 @@ def click(xPos, yPos):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
-click(1000, 750)
+click(1000, 750) # Clicking start again
 
 while not keyboard.is_pressed('esc'):
     frame = pyautogui.screenshot(region=(MIN_X, MIN_Y, MAX_X, MAX_Y))
     
-    for h in range(MIN_X, MAX_X, 5):
-        for v in range(MIN_Y, MAX_Y, 5):
-            if frame.getpixel((h - MIN_X, v - MIN_Y)) == (255, 219, 195):
-                click(h, v)
+    for h in range(MIN_X, MAX_X, 5): # Looping in height
+        for w in range(MIN_Y, MAX_Y, 5): # Looping in width
+            if frame.getpixel((h - MIN_X, w - MIN_Y)) == (255, 219, 195): # If target; click
+                click(h, w)
